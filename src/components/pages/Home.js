77c86faee5597cc8f857cb39f746d8f0b951b865
Luc <lucmccutcheon.home@gmail.com>
2022-12-16 12:34:55 +0000

    added banner

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../App.css';
-import Cards from '../Cards';
+// import Cards from '../Cards';
 import DynamicBackground from '../DynamicBackground';
 import Footer from '../Footer';
 
@@ -8,10 +8,65 @@ function Home() {
   return (
     <>
       <DynamicBackground />
-      <Cards />
+      <Vision />
       <Footer />
     </>
   );
 }
+//<Cards />
 
 export default Home;
+
+function Vision() {
+  const mystyle = {
+  color: "white",
+  backgroundColor: "black",/*"#1C1B1B",*/
+  padding: "2%",
+  fontFamily: "Lobster",
+
+  };
+  const lobster = {
+  fontFamily: "Lobster",
+  };
+  return (
+    <>
+    <style> @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap'); </style>
+      <table style={mystyle}>
+        <th style={{width:"50%", height:"10%"}}><h1 id="vision">The Vision of CAV-Lab for Academic Research</h1>
+        <br></br><br></br>
+        </th>
+        <th></th>
+        <tr>
+        <th><p>To co-exist with all road users in a way that society considers
+         them trustworthy and safe. With an interdisciplinary team of
+          researchers from engineering, social science, and the humanities,
+           CAV-Lab aims to develop a novel highly automated driving platform
+            for HAVs of CCAM using explainable and human-centric AI solutions to
+             provide them greater situational awareness, which is holistic and
+              inclusive of human values, to ensure that the wider anticipated
+               benefits of CCAM are realised.<br></br>
+
+               The report published by the House of Lords (March’17) highlighted
+                that although CAVs have the potential to lower the number of
+                 road fatalities, casualties, and traffic congestion, the
+                  eradication, or near eradication, of human error and traffic
+                   load will only be realised with full automation. Major
+                   international automotive industries believe artificial
+                   intelligence will be the bridge between current automated
+                   vehicles and fully autonomous vehicles.<br></br>
+                However, the question is:</p>
+          </th>
+          <th><br></br>
+            <h3>Insert Video Here!</h3>
+          </th></tr>
+
+          <tr><th>
+                <br></br><br></br>
+                <h2>CAN WE TRUST AI TO DRIVE US?</h2>
+                <br></br><br></br>
+          </th>
+          </tr>
+      </table>
+    </>
+  );
+}
