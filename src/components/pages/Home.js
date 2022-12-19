96508b517fe02bf7884c6fae5db591f95cbd94be
VinalAsodia1999 <vasodia1999@gmail.com>
2022-12-19 17:31:09 +0000

    Replaced table with div

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -32,20 +32,18 @@ function Vision() {
   return (
     <>
     <style> @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap'); </style>
-      <table style={mystyle}>
-        <th style={{width:"50%", height:"10%"}}><h1 id="vision">The Vision of CAV-Lab for Academic Research</h1>
-        <br></br><br></br>
-        </th>
-        <th></th>
-        <tr>
-        <th><p style={{textAlign: "center"}}>To co-exist with all road users in a way that society considers
+    <div style={mystyle}>
+          <div>
+            <h1><br></br>The Vision of CAV-Lab for Academic Research</h1>
+            <br></br><br></br>
+            <p style={{textAlign: "center"}}>To co-exist with all road users in a way that society considers
          them trustworthy and safe. With an interdisciplinary team of
           researchers from engineering, social science, and the humanities,
            CAV-Lab aims to develop a novel highly automated driving platform
             for HAVs of CCAM using explainable and human-centric AI solutions to
              provide them greater situational awareness, which is holistic and
               inclusive of human values, to ensure that the wider anticipated
-               benefits of CCAM are realised.<br></br>
+               benefits of CCAM are realised.<br></br><br></br>
 
                The report published by the House of Lords (March’17) highlighted
                 that although CAVs have the potential to lower the number of
@@ -55,19 +53,15 @@ function Vision() {
                    international automotive industries believe artificial
                    intelligence will be the bridge between current automated
                    vehicles and fully autonomous vehicles.<br></br>
-                However, the question is:</p>
-          </th>
-          <th><br></br>
-            <video src={cavlab_vision} width="600" height="300" controls="controls" autoplay="false" />
-          </th></tr>
-
-          <tr><th>
-                <br></br><br></br>
+                However, the question is: <br></br><br></br></p>
+                
                 <h2>CAN WE TRUST AI TO DRIVE US?</h2>
-                <br></br><br></br>
-          </th>
-          </tr>
-      </table>
+          </div>
+          <div>
+            <h3>Insert Video Here</h3>
+            <br></br>
+          </div>
+      </div>
     </>
   );
 }
