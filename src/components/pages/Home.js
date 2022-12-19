65be0db38f2422448c63bf68005417a98f918c50
Luc <lucmccutcheon.home@gmail.com>
2022-12-19 21:41:12 +0000

    div table and animation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,11 @@
  import React from 'react';
 import '../../App.css';
+import './Pages.css';
 // import Cards from '../Cards';
 import DynamicBackground from '../DynamicBackground';
 import Footer from '../Footer';
 import cavlab_vision from '../../images/cavlab_vision.mp4'
+import ScrollAnimation from 'react-animate-on-scroll';
 
 function Home() {
   return (
@@ -23,17 +25,13 @@ function Vision() {
   color: "white",
   backgroundColor: "black",/*"#1C1B1B",*/
   padding: "2%"};
-  // fontFamily: "Lobster",
 
-  // };
-  // const lobster = {
-  // fontFamily: "Lobster",
-  // };
   return (
     <>
-    <style> @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap'); </style>
-    <div style={mystyle}>
-          <div>
+    <div style={mystyle} class="grid-container">
+    <ScrollAnimation animateIn="fadeIn">
+
+          <div id="grid-item">
             <h1><br></br>The Vision of CAV-Lab for Academic Research</h1>
             <br></br><br></br>
             <p style={{textAlign: "center"}}>To co-exist with all road users in a way that society considers
@@ -54,10 +52,10 @@ function Vision() {
                    intelligence will be the bridge between current automated
                    vehicles and fully autonomous vehicles.<br></br>
                 However, the question is: <br></br><br></br></p>
-                
+
                 <h2>CAN WE TRUST AI TO DRIVE US?</h2>
-          </div>
-          <div>
+          </div></ScrollAnimation>
+          <div class="grid-item">
             <h3>Insert Video Here</h3>
             <br></br>
           </div>
@@ -65,3 +63,41 @@ function Vision() {
     </>
   );
 }
+/*
+<>
+<style> @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap'); </style>
+<div style={mystyle} id="grid">
+<Grid container spacing={2}>
+  <Grid xs={8}>
+  <Item>
+      <div>
+        <h1><br></br>The Vision of CAV-Lab for Academic Research</h1>
+        <br></br><br></br>
+        <p style={{textAlign: "center"}}>To co-exist with all road users in a way that society considers
+     them trustworthy and safe. With an interdisciplinary team of
+      researchers from engineering, social science, and the humanities,
+       CAV-Lab aims to develop a novel highly automated driving platform
+        for HAVs of CCAM using explainable and human-centric AI solutions to
+         provide them greater situational awareness, which is holistic and
+          inclusive of human values, to ensure that the wider anticipated
+           benefits of CCAM are realised.<br></br><br></br>
+
+           The report published by the House of Lords (March’17) highlighted
+            that although CAVs have the potential to lower the number of
+             road fatalities, casualties, and traffic congestion, the
+              eradication, or near eradication, of human error and traffic
+               load will only be realised with full automation. Major
+               international automotive industries believe artificial
+               intelligence will be the bridge between current automated
+               vehicles and fully autonomous vehicles.<br></br>
+            However, the question is: <br></br><br></br></p>
+
+            <h2>CAN WE TRUST AI TO DRIVE US?</h2>
+      </div></Item></Grid>
+      <Grid xs={4}>
+      <Item><div id="stats">
+        <h3>Insert Video Here</h3>
+        <br></br>
+      </div></Item></Grid></Grid>
+  </div>
+</>*/
