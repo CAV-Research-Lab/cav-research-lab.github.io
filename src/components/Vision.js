c99898da97f92efbad7a94d5b90e4cc60509c40a
VinalAsodia1999 <vasodia1999@gmail.com>
2023-01-13 12:11:36 +0000

    Fixed video position in Vision component

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -5,18 +5,14 @@ import YoutubeEmbed from "./YoutubeEmbed";
 import ScrollAnimation from 'react-animate-on-scroll';
 
 function Vision() {
-    const mystyle = {
-    color: "white",
-    backgroundColor: "#333333",/*"#1C1B1B",*/
-    padding: "5%"};
-  
+
     return (
       <>
-      <div style={mystyle} id="vision-grid-container">
+      <div id="vision-grid-container">
       <ScrollAnimation animateIn="fadeIn">
   
             <div id="grid-item">
-              <p style={{textAlign: "center"}}>CAV-Lab envisions a world in which 
+              <p>CAV-Lab envisions a world in which 
               humans and autonomous systems are capable of safely and harmoniously 
               co-existing together. <br></br><br></br>
 
@@ -29,7 +25,7 @@ function Vision() {
                  <br></br><br></br></p>
 
             </div></ScrollAnimation>
-            <div class="video_wrapper">
+            <div id="video_wrapper">
               <YoutubeEmbed embedId="3b9qXX7sG50" />
             </div>
         </div>
--- src/components/YoutubeEmbed.js
+++ src/components/YoutubeEmbed.js
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
-      width="500"
-      height="300"
+      width="525"
+      height="325"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
