2dfab75e6b8972d7297f4764e66fc4c5fa5cb5f5
VinalAsodia1999 <vasodia1999@gmail.com>
2023-01-26 15:25:00 +0000

    Added responsiveness to vision and about us sections of homepage.

--- src/components/Posts/NewsPosts.js
+++ src/components/Posts/NewsPosts.js
@@ -18,7 +18,7 @@ const newsPosts = [
 	title: "Vision-based Reinforcement Learning for Emergency Braking",
 	body: `Our PhD student at CAV-Lab, Vinal Asodia, designed an emergency braking system using a vision-based reinforcement learning technique. However, during testing, the car exhibited an unexpected behavior: it brakes twice in emergency scenarios when a pedestrian is detected. This is likely a feature that was unintentionally learned by the vehicle. It is important to investigate why the car is braking twice and determine if this behavior is beneficial or detrimental before implementing AI-powered autonomous vehicles in real-world applications. This highlights the need to address such questions and challenges to ensure the technology is trustworthy.`,
 	videoUrl:
-		require('https://youtu.be/I_eGm44ncg8'),
+		'https://youtu.be/I_eGm44ncg8',
 
 	},
 	
