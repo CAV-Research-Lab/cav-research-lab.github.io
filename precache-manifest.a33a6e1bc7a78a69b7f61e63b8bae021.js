2edeec0bfcec5b8a5ba96489ddbf3871fc96d909
VinalAsodia1999 <vasodia1999@gmail.com>
2023-02-05 19:53:17 +0000

    Updates

---
+++ precache-manifest.a33a6e1bc7a78a69b7f61e63b8bae021.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b95998180a34597e7f44652d332674",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aa437dd6fdf3e881a9d",
+    "url": "/static/css/main.02402281.chunk.css"
+  },
+  {
+    "revision": "123fa3a274363f073287",
+    "url": "/static/js/2.6e3272c4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6e3272c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa437dd6fdf3e881a9d",
+    "url": "/static/js/main.d5bc921d.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  },
+  {
+    "revision": "b57b1a7eacbc49e2e2e69afe3cda34e3",
+    "url": "/static/media/CAV-LAB_logo_white.b57b1a7e.png"
+  },
+  {
+    "revision": "ea830f0881e1e320f3ccede9a814985b",
+    "url": "/static/media/CW.ea830f08.png"
+  },
+  {
+    "revision": "43139eb7b1347cfa6f01de3f54a52016",
+    "url": "/static/media/background_video.43139eb7.mp4"
+  },
+  {
+    "revision": "fc7f4f203b1cfff83c262810f8b2d632",
+    "url": "/static/media/barkin_dagda.fc7f4f20.png"
+  },
+  {
+    "revision": "a5d56aafdf1cc46b85767d7dc72e272f",
+    "url": "/static/media/glen_mason.a5d56aaf.jpg"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4f673dc3f81524c7e1aff27371cbc923",
+    "url": "/static/media/mustafa_yildirim.4f673dc3.png"
+  },
+  {
+    "revision": "77e698c41a401f830f50842e8a9997db",
+    "url": "/static/media/saber_fallah.77e698c4.jpeg"
+  },
+  {
+    "revision": "b30fc4cecaa1fb8d82b29d4ec7d94c21",
+    "url": "/static/media/sponsors.b30fc4ce.PNG"
+  }
+]);
