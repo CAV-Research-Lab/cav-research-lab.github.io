5e660973a010cb0dd0c9b2b36597c43642be6606
Luc <lucmccutcheon.home@gmail.com>
2023-03-10 16:10:55 +0000

    Updates

---
+++ precache-manifest.ca5f6a5c375a3703e50225020b07cd06.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2082998f172f11f4dda7e7ee2da6b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb5bd912dfb88432888",
+    "url": "/static/css/main.120f5b7f.chunk.css"
+  },
+  {
+    "revision": "5694ddd43611a47a2e80",
+    "url": "/static/js/2.884eff31.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.884eff31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb5bd912dfb88432888",
+    "url": "/static/js/main.64a26291.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  },
+  {
+    "revision": "b57b1a7eacbc49e2e2e69afe3cda34e3",
+    "url": "/static/media/CAV-LAB_logo_white.b57b1a7e.png"
+  },
+  {
+    "revision": "ea830f0881e1e320f3ccede9a814985b",
+    "url": "/static/media/CW.ea830f08.png"
+  },
+  {
+    "revision": "944f17e7162b3bf3bd9e177d09bdb5e7",
+    "url": "/static/media/Socio-Drive.944f17e7.png"
+  },
+  {
+    "revision": "43139eb7b1347cfa6f01de3f54a52016",
+    "url": "/static/media/background_video.43139eb7.mp4"
+  },
+  {
+    "revision": "fc7f4f203b1cfff83c262810f8b2d632",
+    "url": "/static/media/barkin_dagda.fc7f4f20.png"
+  },
+  {
+    "revision": "a5d56aafdf1cc46b85767d7dc72e272f",
+    "url": "/static/media/glen_mason.a5d56aaf.jpg"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "2e94fca19d4b70ee12656c953f76ce60",
+    "url": "/static/media/luc_mccutcheon.2e94fca1.jpg"
+  },
+  {
+    "revision": "4f673dc3f81524c7e1aff27371cbc923",
+    "url": "/static/media/mustafa_yildirim.4f673dc3.png"
+  },
+  {
+    "revision": "77e698c41a401f830f50842e8a9997db",
+    "url": "/static/media/saber_fallah.77e698c4.jpeg"
+  },
+  {
+    "revision": "b30fc4cecaa1fb8d82b29d4ec7d94c21",
+    "url": "/static/media/sponsors.b30fc4ce.PNG"
+  }
+]);
