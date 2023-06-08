d51aaef807e557246b955699ce1db38b9fbcd8ba
CAV-Lab <cavresearchlab@gmail.com>
2023-06-08 23:28:57 +0100

    Automated Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
   window.addEventListener('resize', showButton);
 
   // const logoStyle = {
-  //   marginTop: "-0.08%"
+  //   marginTop: "-5%"
   // };
   // toggleBtn.addEventListener("click", function() {
   //     if (myList.style.display === "none") {
